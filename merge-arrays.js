743d2e026bbb09e992b30137c59a0ac46fd9f006
sarah-zhan <yizh.contact@gmail.com>
2022-10-02 15:50:09 -0700

    merge array function

---
+++ merge-arrays.js
@@ -0,0 +1,8 @@
+const merge = function (array1, array2) {
+  array1.push(...array2) //merge array 2 to array1
+  return array1.sort() //sort it out
+}
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]), "=?", [1, 2, 3, 4, 4, 5, 6]);
+console.log(merge([4], [2, 5, 8]), "=?", [2, 4, 5, 8]);
+console.log(merge([1, 2, 6], []), "=?", [1, 2, 6]);
