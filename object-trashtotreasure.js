100479eb9e8e9bbd96c0ba7dba17cf5086d4aa06
sarah-zhan <yizh.contact@gmail.com>
2022-10-04 21:54:20 -0700

    function

---
+++ object-trashtotreasure.js
@@ -0,0 +1,13 @@
+const smartGarbage = function (trash, bins) {
+  const keys = Object.keys(bins);
+  if (trash === "waste") {
+    bins.waste += 1;
+  } else if (trash === "recycling") {
+    bins.recycling += 1;
+  } else if (trash === "compost") {
+    bins.compost += 1;
+  }
+  return bins;
+}
+
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }))
